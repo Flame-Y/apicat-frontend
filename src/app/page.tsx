d44cecb540977800c44ba58e14bd831c0f6cb392
Flame-Y <1248993272@qq.com>
2023-10-04 13:58:19 +0800

    :sparkles: feat(nav): 完成侧边导航栏

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-export default function Home() {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">111</button>
-        </>
-    );
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+    redirect('/dashboard');
+    return <h1>Hello, Home page!</h1>;
 }
